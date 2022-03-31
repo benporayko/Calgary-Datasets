ccdc719a906436675aba1960298377633622fb2e
bpSAIT <78457540+bpSAIT@users.noreply.github.com>
2022-03-30 20:54:18 -0600

    IncidentSearch Component

--- src/components/IncidentSearch.js
+++ src/components/IncidentSearch.js
@@ -1,9 +1,34 @@
 import React from "react";
 
 function IncidentSearch () {
+
+    const quadrantHandler = (event) => {
+        // TODO: load traffic incident data into an array and search based on select quadrant
+        console.log(event.target.value);
+    }
+
     return (
         <div>
-            <h2>Test</h2>
+            <h2>Searching Traffic Incidents</h2>
+            <h4>Search by Quadrant:</h4>
+            <div onChange={quadrantHandler}>
+                <label>
+                    <input type="radio" value="NW" name="quadrant"></input>
+                    <span>NW</span>
+                </label>
+                <label>
+                    <input type="radio" value="NE" name="quadrant"></input>
+                    <span>NE</span>
+                </label>
+                <label>
+                    <input type="radio" value="SE" name="quadrant"></input>
+                    <span>SE</span>
+                </label>
+                <label>
+                    <input type="radio" value="SW" name="quadrant"></input>
+                    <span>SW</span>
+                </label>
+            </div>
         </div>
     )
 }
